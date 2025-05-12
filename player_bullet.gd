4c47571945868156e6ae4be7588963ffef66cb3c
Garrett Hobson <60861250+GartTheSkeleton@users.noreply.github.com>
2025-05-12 16:23:28 -0500

    Update player_bullet.gd

--- player_bullet.gd
+++ player_bullet.gd
@@ -7,4 +7,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if global_position.y < 0:
+		queue_free()
+	
+	global_position.y -
